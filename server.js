553ace545f44a5c175d96fbd397aec28dd751b79
mounaspriya <psjan96@gmail.com>
2025-08-07 01:00:19 +0530

    fix: added express.json and updated CORS for vercel

--- server.js
+++ server.js
@@ -7811,7 +7811,7 @@ if (!fs.existsSync(uploadsDir)) {
 // === Middleware ===
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://my-frontend-steel-mu.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
