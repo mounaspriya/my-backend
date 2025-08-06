02d90f97ff6d11bdb5228435ba296a5292e1e298
mounaspriya <psjan96@gmail.com>
2025-08-07 01:16:40 +0530

    fix: added express.json and updated CORS for vercel

--- server.js
+++ server.js
@@ -7817,6 +7817,8 @@ app.use(
     credentials: true,
   }),
 )
+
+app.options("*", cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static("uploads"))
